d0d984e518460c52c817263e9c7283d7d2334ea7
Tor <tor.j.robinson@gmail.com>
2014-03-02 17:25:19 -0700

    Adding animations, WIP

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 <h1>floatlabel.js</h1>
+
+Per the example files included, throw some inputs into a container element and style them appropriately for the design pattern (leave some space on top of the inputs).
+
+Include jQuery, floatlabel.jquery.js, and floatlabel.jquery.css, then apply the plugin to the container element.
--- floatlabel.jquery.js
+++ floatlabel.jquery.js
@@ -13,23 +13,35 @@ Based on the float label pattern by Matt D. Smith (http://dribbble.com/shots/125
 
        return this.each( function() {
            $(this).children('input').each(function(){
-                //prepare
+               
+                //wrap and add label
                 $(this).wrap('<span class="fltlblHolder">');
                 $(this).before('<label for="'+$(this).attr('id')+'" class="fltlbl fltlblHide">'+$(this).attr('placeHolder')+'</label>');
                
                $(this).prev('label').addClass(settings.customClass);
                $(this).prev('label').css({
-                   left :settings.offsetx,
-                   top  :settings.offsety
+                   left :settings.leftOffset,
+                   top  :settings.topOffset
                });
+               /////////////////////////
+               
+               //transfer margins to the container
+               $(this).parent('.fltlblHolder').css({
+                   margin:  $(this).css('margin')
+               });
+               $(this).css({'margin':0});
+               $(this).prev('label').css({'top':'0px'});
+               ///////////////////////////////////
                 
                //events
                 $(this).keyup(function(ev){
                     if($(this).val()!=''){
-                        $(this).prev('label').removeClass('fltlblHide');   
+                        $(this).prev('label').removeClass('fltlblHide'); 
+                        $(this).prev('label').css({'top':settings.topOffset});
                     }
                     else{
                         $(this).prev('label').addClass('fltlblHide');
+                        $(this).prev('label').css({'top':'0px'});
                     }
                 });
                
