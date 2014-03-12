74ed9750431bbee7281b86cbf07a30029a51dea3
Tor <torrobinson@users.noreply.github.com>
2014-03-11 20:53:57 -0600

    Code cleanup, added animationSpeed

--- floatlabel.jquery.js
+++ floatlabel.jquery.js
@@ -7,7 +7,6 @@ Based on the float label pattern
 by Matt D. Smith 
 http://dribbble.com/shots/1254439--GIF-Float-Label-Form-Interaction
 */
-
 jQuery.support.placeholder = (function(){
             var i = document.createElement('input');
             return 'placeholder' in i;
@@ -20,41 +19,35 @@ jQuery.support.placeholder = (function(){
             topOffset      : '0px',
             fontSize       : '9px',
             color          : '#0b7dfd',
-            colorInactive  : '#c9c9c9'
+            colorInactive  : '#c9c9c9',
+            animationSpeed: 125
         }, options);
-
         return this.each( function() {
            $(this).find('input').each(function(){
                var width = $(this).outerWidth();
                var widthOnly = parseInt($(this).css('width'),10);
                var height = $(this).outerHeight() - parseInt($(this).css('margin-top'),10)+parseInt($(this).css('margin-bottom'),10);
                height = parseInt(parseInt(settings.topOffset,10) + height*-1).toString()+'px';
-               
                 //wrap input in container and add label
                 $(this).wrap('<span class="floatLabelHolder">');
                 $(this).before('<label for="'+$(this).attr('id')+'" class="floatLabel floatLabelHide">'+$(this).attr('placeHolder')+'</label>');
-               
                $(this).siblings('label').css({
                    left          :settings.leftOffset,
                    top           :height,
                    'font-size'   :settings.fontSize,
                    'color'       :settings.colorInactive
                });
-               
                if(!jQuery.support.placeholder){
                     $(this).before('<span class="placeholder">'+$(this).attr('placeHolder')+'</span>');
                     $(this).siblings('.placeholder').css({
                         'left': (parseInt($(this).css('padding-left'),10) + parseInt($(this).css('border-left-width'),10))+'px',
                         'top': (parseInt($(this).css('padding-top'),10) + parseInt($(this).css('border-top-width'),10))+'px',
                     });
-                   
                    //passthrough click events
-                   $(this).siblings('.placeholder').click(function(e){
+ $(this).siblings('.placeholder').click(function(){
                         $(this).siblings('input').focus();
                    });
-                   
                }
-               
                //transfer input width and margin to container
                $(this).parent('.floatLabelHolder').css({
                    'margin-top':  $(this).css('margin-top'),
@@ -70,8 +63,8 @@ jQuery.support.placeholder = (function(){
                });
                 
                //bind events
-                $(this).keyup(function(ev){
-                    if($(this).val()!=''){
+                $(this).keyup(function(){
+                    if($(this).val()!==''){
                         if(!jQuery.support.placeholder) $(this).siblings('.placeholder').hide();
                         $(this).siblings('label').css({display:'inline-block'}).animate({
                             top    :height,
@@ -92,7 +85,7 @@ jQuery.support.placeholder = (function(){
                });
                $(this).blur(function(){
                     $(this).siblings('label').removeClass('active').css('color',settings.colorInactive);
-               });               
+               });
            });
         });
     }
